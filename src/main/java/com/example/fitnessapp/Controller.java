28a376c651d0806858c2b3c402e7272d5be73ff5
Selina Frenes <stfresel@bx.fallmerayer.it>
2023-04-25 10:26:23 +0200

    alles noch in javaFX umwandeln

--- src/main/java/com/example/fitnessapp/Controller.java
+++ src/main/java/com/example/fitnessapp/Controller.java
@@ -1,14 +1,56 @@
 package com.example.fitnessapp;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private final double abstandZuRand = 40;
+    private Pane pane;
+    private Scene scene;
+    private Stage stage;
+
+    public Controller(Stage stage, Scene scene,Pane pane) {
+        this.pane = pane;
+        this.scene = scene;
+        this.stage = stage;
+    }
+
+    /**
+     * Ruft die Funktion auf, die eine neue oder bereitsbestehende Zutat zum Meal hinzufügt, auf.
+     * Sie wird aufgerufen, sobald man sich im Layout Tagebucheintrag befindet.
+     */
+    public void addZutaten2Meal(){
+        Button addZutat2MealBtn = new Button("+");
+        addZutat2MealBtn.setPrefHeight(30);
+        addZutat2MealBtn.setPrefWidth(30);
+        System.out.println("H" + addZutat2MealBtn.getHeight());
+        addZutat2MealBtn.setLayoutY(pane.getHeight()-addZutat2MealBtn.getHeight()-abstandZuRand);   //height und width ist immer 0.0 ???
+        addZutat2MealBtn.setLayoutX(pane.getWidth()-addZutat2MealBtn.getWidth()-abstandZuRand);
+        addZutat2MealBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("+");
+
+            }
+        });
+        pane.getChildren().removeAll();
+        pane.getChildren().add(addZutat2MealBtn);
+    }
+
+    private void loadZutatenScene(){
+        Pane zutatenPane = new Pane();
+        zutatenPane.setPrefWidth(pane.getWidth());
+        zutatenPane.setPrefHeight(pane.getHeight());
+        // zutaten pane layout
+
+        Scene zutatenScene = new Scene(zutatenPane);
+        stage.setScene(zutatenScene);
     }
 }
--- src/main/java/com/example/fitnessapp/Main.java
+++ src/main/java/com/example/fitnessapp/Main.java
@@ -3,6 +3,7 @@ package com.example.fitnessapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,14 +11,20 @@ import java.util.ArrayList;
 
 public class Main extends Application {
     static ArrayList<Zutat> gespeicherteZutaten = new ArrayList<>();
+
+    static Pane pane = new Pane();
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        pane.setPrefHeight(500);
+        pane.setPrefWidth(500);
+        Scene scene = new Scene(pane);
         stage.setTitle("Miau!");
         stage.setScene(scene);
         stage.show();
-        Controller controller = new Controller();
+        Controller controller = new Controller(stage, scene, pane);
+        controller.addZutaten2Meal();
         Meal m = new Meal();
 
         //m.neueZutatErstellen("Honig", 20, 100, new Naehrwerte(200, 200, 200, 200));
